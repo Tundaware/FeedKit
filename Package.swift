9ea5d1a85d5d275804d9c46897043044f60cfebf
George Cox <george@tundaware.com>
2021-07-12 17:15:00 -0400

    dynamic lib

--- Package.swift
+++ Package.swift
@@ -1,7 +1,20 @@
+// swift-tools-version:5.4
+
 import PackageDescription
 
 let package = Package(
     name: "FeedKit",
+    products: [
+      .library(
+        name: "FeedKit",
+        type: .dynamic,
+        targets: ["FeedKit"]
+      )
+    ],
     dependencies : [],
-    exclude: ["Tests"]
+    targets: [
+      .target(
+        name: "FeedKit"
+      )
+    ]
 )
